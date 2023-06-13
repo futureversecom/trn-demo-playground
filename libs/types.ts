ab5f4f6239536a0f73579c779abb4b658233f3fe
Aidan Starke <starke.aidan@gmail.com>
2023-06-14 09:58:58 +1200

    Fix `RootTx` types

--- libs/types.ts
+++ libs/types.ts
@@ -1,9 +1,14 @@
 import type { Web3Provider } from "@ethersproject/providers";
 import type { SubmittableExtrinsic } from "@polkadot/api/types";
+import type { u32 } from "@polkadot/types/primitive";
 import type { ISubmittableResult } from "@polkadot/types/types";
 import type { Web3ContextType } from "@web3-react/core";
 
 export type Demo = "EvmFeeProxy";
+
+export interface Result extends ISubmittableResult {
+	blockNumber: u32;
+}
 
 export type Extrinsic = SubmittableExtrinsic<"promise", ISubmittableResult>;
 
